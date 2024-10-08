483f5a9ab1e392365809e672e8fed3785ffdd1aa
1212-5 <1212-5@DESKTOP-NK4O6IE>
2024-10-08 13:18:47 +0300

    Add project files.

---
+++ MyFirstMVC/Controllers/TipController.cs
@@ -0,0 +1,28 @@
+﻿using MyFirstMVC.Models;
+using MyFirstMVC.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Controllers
+{
+    public class TipController
+    {
+        Display display = new Display();
+        Tip tip = new Tip();
+
+        public TipController()
+        {
+            display.Input();
+
+            tip.Amount = display.Amount;
+            tip.Percent = display.Percent;
+            display.TotalSum = tip.CalculateTotal();
+            display.TipSum = tip.CalculateTip();
+
+            display.Output();
+        }
+    }
+}
---
+++ MyFirstMVC/Views/Display.cs
@@ -0,0 +1,33 @@
+﻿using MyFirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstMVC.Views
+{
+    public class Display
+    {
+
+        public double Amount { get; set; }
+        public double Percent { get; set; }
+        public double TipSum { get; set; }
+        public double TotalSum { get; set; }
+
+        public void Input()
+        {
+            Console.Write("Въведете сумата на сметката: ");
+            double sum = double.Parse(Console.ReadLine());
+            this.Amount = sum;
+            Console.Write("Въведете процент: ");
+            double percent = double.Parse(Console.ReadLine());
+            this.Percent = percent;
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Бакшишът за сервитьора е {this.TipSum}лв.");
+            Console.WriteLine($"Сумата за плащане е {this.TotalSum}лв.");
+        }
+    }
+}
